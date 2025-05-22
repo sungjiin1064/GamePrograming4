84317284d6f95d8a9c21cfe005fab0654450ce8b
sungjiin1064 <psj10645001@gmail.com>
2025-05-22 11:38:22 +0900

    (05-21) 노션을 활용하여 게임 프로젝트 관리하기

--- Ch6_TownRPG/GameModel.h
+++ Ch6_TownRPG/GameModel.h
@@ -2,6 +2,8 @@
 
 #include <iostream>
 
+using namespace std;
+
 class Combat
 {
 private:
@@ -11,15 +13,57 @@ public:
 
 	virtual void attack(Combat& target) = 0;
 	virtual void takeDamage(int damage) = 0;
-	virtual void getHealth() = 0;
+	virtual int getHealth() = 0;
 };
 
 class Player : public Combat
 {
+private:
+	int _health;
+	int _ATK;
 
+public:
+	Player(int health, int ATK) : _health(health), _ATK(ATK) {}
+
+	void attack(Combat& target) override
+	{
+		target.takeDamage(_ATK);
+	}
+
+	void takeDamage(int damage) override
+	{
+		_health -= damage;
+		cout << "Player가 " << damage << " 의 피해를 입었습니다" << endl;
+	}
+
+	int getHealth() override
+	{
+		return _health;
+	}
 };
 
-class Monster
+class Monster : public Combat
 {
+private:
+	int _health;
+	int _ATK;
 
+public:
+	Monster(int health, int ATK) : _health(health), _ATK(ATK) {}
+
+	void attack(Combat& target) override
+	{
+		target.takeDamage(_ATK);
+	}
+
+	void takeDamage(int damage) override
+	{
+		_health -= damage;
+		cout << "Monster가 " << damage << " 의 피해를 입었습니다" << endl;
+	}
+
+	int getHealth() override
+	{
+		return _health;
+	}
 };
--- Ch6_TownRPG/main.cpp
+++ Ch6_TownRPG/main.cpp
@@ -3,5 +3,18 @@
 
 int main()
 {
+	Player player(100, 10);
+	Monster monster(80, 5);
 
+	while (player.getHealth() > 0 && monster.getHealth() > 0)
+	{
+		player.attack(monster);
+		if (monster.getHealth() > 0)
+		{
+			monster.attack(player);
+		}
+	
+	}
+
+	cout << "전투가 종료되었습니다." << endl;
 }
