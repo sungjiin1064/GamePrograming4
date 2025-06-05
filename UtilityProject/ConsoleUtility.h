3b98c61950319dbc84b14587759c5d41e43fc73f
sungjiin1064 <psj10645001@gmail.com>
2025-06-05 17:47:05 +0900

    (06-05)

--- UtilityProject/ConsoleUtility.h
+++ UtilityProject/ConsoleUtility.h
@@ -40,8 +40,40 @@ public:
 
 		auto end = std::chrono::high_resolution_clock::now();
 		std::chrono::duration<double, std::milli> duration = end - start;
-		cout << duration.count() << "ms시간이 걸렸습니다." << endl;
-		
+		cout << duration.count() << "ms시간이 걸렸습니다." << endl;		
 	}
 
+	static void SetColor(int colorID)
+	{
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), colorID);
+	}
+
+	static void SetWindowSize(int cols, int lines)
+	{
+		system("title PacManX");
+		char cmd[30];
+		sprintf_s(cmd, "mode con cols=%d lines=%d", cols, lines);
+		system(cmd);
+	}
 };
+
+//const int WHITE_COLOR = FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY;
+//const int RED_COLOR = FOREGROUND_RED | FOREGROUND_INTENSITY;
+//const int BLUE_COLOR = FOREGROUND_BLUE | FOREGROUND_INTENSITY;
+//const int GREEN_COLOR = FOREGROUND_GREEN | FOREGROUND_INTENSITY;
+//const int BLUE_GREEN_COLOR = FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY;
+//const int BLUE_LITE_COLOR = FOREGROUND_BLUE | FOREGROUND_GREEN;
+//const int YELLOW_COLOR = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY;
+//const int YELLOW_LITE_COLOR = FOREGROUND_RED | FOREGROUND_GREEN;
+//const int GRAY_COLOR = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE;
+//const int PINK_COLOR = FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_INTENSITY;
+//
+//const int TITLE_COLOR = BACKGROUND_INTENSITY | BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE;
+//const int SELECTED_COLOR = BACKGROUND_GREEN | BACKGROUND_BLUE | WHITE_COLOR;
+//const int THEME_COLOR = BLUE_GREEN_COLOR;
+//const int GAMENAME_COLOR = FOREGROUND_GREEN;
+//const int HIGHLIGHT_COLOR = BACKGROUND_GREEN | WHITE_COLOR;
+//const int PAUSE_COLOR = BACKGROUND_RED | WHITE_COLOR;
+//const int LOST_COLOR = BACKGROUND_RED | BACKGROUND_INTENSITY | WHITE_COLOR;
+//const int WIN_COLOR = BACKGROUND_GREEN | BACKGROUND_INTENSITY | WHITE_COLOR;
+//const int MODE_COLOR = TITLE_COLOR | FOREGROUND_GREEN;
